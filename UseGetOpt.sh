8476d1c26196a939d371314e080ad35d55b2cac1
dsriram <dhiraj.sriram@taistech.com>
2019-03-20 12:29:54 -0500

    first commit

---
+++ UseGetOpt.sh
@@ -0,0 +1,19 @@
+#'brew install jq'
+sample=`cat package.json`
+arr=()
+arr=($(echo "${sample}" | jq -r ' .scripts |keys | join(" ")'))
+_UseGetOpt-2 ()
+{
+  local cur
+  cur=${COMP_WORDS[COMP_CWORD]}
+  COMPREPLY=( $( compgen -W '${arr[*]}' $cur))
+  case "$cur" in
+    -*)
+    COMPREPLY=( $( compgen -W '-a -d -f -l -t -h --aoption --debug \
+                               --file --log --test --help --' -- $cur ) );
+  esac
+  return 0
+}
+
+complete -F _UseGetOpt-2 -o filenames npm run
+
