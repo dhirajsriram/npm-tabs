d1826e466696f9d23a57b565b118e208c4fcf270
Dhiraj Sriram <ds@Dhirajs-Mini.attlocal.net>
2019-03-28 22:27:51 -0500

    removed unnecessary dependency and moded package.json

--- npm-tabs.sh
+++ npm-tabs.sh
@@ -1,4 +1,12 @@
-#'brew install jq' Installation for mac // Uncomment the first time
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+if hash jq>/dev/null; then
+    echo -e "${GREEN}✔ jq dependency found${NC}" 
+else
+    echo -e "${RED}No jq dependency found , Installing...${NC}"
+    brew install jq
+fi
 sample=`cat package.json`
 arr=()
 arr=($(echo "${sample}" | jq -r ' .scripts |keys | join(" ")'))
@@ -12,8 +20,9 @@ _UseGetOpt-2 ()
     COMPREPLY=( $( compgen -W '-a -d -f -l -t -h --aoption --debug \
                                --file --log --test --help --' -- $cur ) );
   esac
+  
   return 0
 }
 
 complete -F _UseGetOpt-2 -o filenames npm run
-
+echo -e "${GREEN}✔ Automcomplete embedded to terminal. Try 'npm run ' and tab to start${NC}" 
